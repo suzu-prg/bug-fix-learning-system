eb19344e0779dd16123020cf0181349f682bffd4
suzu <suzu.prg@gmail.com>
2020-10-07 13:06:03 +0900

    build environment

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import { render } from 'react-dom';
+import React from 'react';
+
+render(<p>Hello World3</p>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+const mode = isProduction ? 'production' : 'development';
+
+module.exports = {
+  entry: './src/index.tsx',
+  target: 'web',
+  mode,
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: false,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('src', `index.ejs`)
+    }),
+  ],
+  devtool: isProduction ? false : 'eval-source-map',
+  devServer: {
+    contentBase: [path.resolve('dist')],
+    historyApiFallback: true,
+    open: true,
+  },
+};
