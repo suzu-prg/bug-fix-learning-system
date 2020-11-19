cd9f371d78ce56614dea2a9b1b6c9156c915cdf3
suzu <suzu.prg@gmail.com>
2020-11-20 01:14:01 +0900

    test firebaseUI React Components

--- src/SignInScreen.tsx
+++ src/SignInScreen.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import firebase from 'firebase';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+// Configure Firebase.
+const config = {
+    apiKey: 'AIzaSyAeue-AsYu76MMQlTOM-KlbYBlusW9c1FM',
+    authDomain: 'myproject-1234.firebaseapp.com',
+    // ...
+};
+firebase.initializeApp(config);
+
 const uiConfig = {
     signInFlow: 'popup',
     signInSuccessUrl: "/",
@@ -8,14 +17,14 @@ const uiConfig = {
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
         firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-        firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+        // firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
     ],
 }
 
-export const SignInScreen:React.FC = () => {
+export const SignInScreen: React.FC = () => {
+
     return (
         <div>
             <p>Please sign-in:</p>
