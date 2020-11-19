a57a8d0e1961c2d1be9c725bb03f46e7fa8f9abe
suzu <suzu.prg@gmail.com>
2020-11-20 00:29:56 +0900

    add firebaseUI React Components

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,56 @@ import {
     useParams
 } from "react-router-dom";
 import { Problem } from './Problem';
+import firebase from 'firebase';
+import { SignInScreen } from './SignInScreen';
+import { render } from 'react-dom';
 
 export const App: React.FC = () => {
-    return (
-        <Router>
+    state = {
+        loading: true,
+        user: null
+    };
+    const componentDidMount = (): void => {
+        firebase.auth().onAuthStateChanged(user => {
+            this.setState({
+                loading: false,
+                user: user
+            });
+        });
+    };
+
+    const logout = (): void => {
+        firebase.auth().signOut();
+    }
+
+    const render = (): void => {
+        if(this.state.loading) return <div>loading</div>;
+        return (
             <div>
-                <Switch>
-                    <Route path="/problem/:problemId">
-                        <Problem />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
+                Uername: {this.state.user && this.state.user.displayName}
+                <br />
+                {this.state.user ? 
+                (<button onClick={this.logout}>Logout </button>) :
+                (<SignInScreen />)
+            }
             </div>
-        </Router>
-    )
+        );
+    }
+
+    // return (
+    //     <Router>
+    //         <div>
+    //             <Switch>
+    //                 <Route path="/problem/:problemId">
+    //                     <Problem />
+    //                 </Route>
+    //                 <Route path="/">
+    //                     <Home />
+    //                 </Route>
+    //             </Switch>
+    //         </div>
+    //     </Router>
+    // )
 }
 
 function Home() {
