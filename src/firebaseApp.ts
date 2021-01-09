725ff05f9f3d650bf37eb3b4ed4410f1f62d0732
Sakamoto, Kazunori <exkazuu@gmail.com>
2021-01-09 12:24:43 +0900

    build: don't bundle react and firebase

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,5 +1,4 @@
 import firebase from "firebase/app";
-import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBLCJEWmOK7zqDah-h-ik3qPKQDsJgdNIk",
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+const packageJson = require('./package.json');
+
 const isProduction = process.env.NODE_ENV === "production";
 const mode = isProduction ? "production" : "development";
 
@@ -11,6 +13,12 @@ module.exports = {
     publicPath: "/",
     filename: "[chunkhash].bundle.mjs",
     chunkFilename: "[chunkhash].chunk.mjs",
+  },
+  externals: {
+    firebase: 'firebase',
+    'firebase/app': 'firebase',
+    react: 'React',
+    'react-dom': 'ReactDOM',
   },
   mode,
   resolve: {
@@ -37,7 +45,12 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve("src", `index.ejs`),
+      template: path.resolve('src', `index.ejs`),
+      templateParameters: {
+        isProd: isProduction,
+        firebaseVersion: getDependencyVersion('firebase'),
+        reactVersion: getDependencyVersion('react'),
+      },
     }),
   ],
   devtool: isProduction ? false : "eval-source-map",
@@ -45,5 +58,10 @@ module.exports = {
     contentBase: [path.resolve("dist")],
     historyApiFallback: true,
     open: true,
+    port: 8080,
   },
 };
+
+function getDependencyVersion(dependencyName) {
+  return packageJson.dependencies[dependencyName].replace('^', '');
+}
